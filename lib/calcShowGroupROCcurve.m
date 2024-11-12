58f612ed81c15101a4cdcd21cc8645391e359ec8
takuto-okuno-riken <66657250+takuto-okuno-riken@users.noreply.github.com>
2024-11-12 21:06:34 +0900

    added analysis scripts.

---
+++ lib/calcShowGroupROCcurve.m
@@ -0,0 +1,51 @@
+%%
+% Calculate and show ROC curve with group data matrix
+% returns X, Y (node x elements) and AUC (node x 1)
+% input:
+%  gmb         Ground Truth binary matrix (node x elements)
+%  tm          evaluating group data matrix (node x elements)
+%  groupStr    group name for ROC curve plot
+%  isShow      flag to plot ROC curve
+%  col         color of ROC curve
+
+function [X, Y, AUC] = calcShowGroupROCcurve(gmb, tm, groupStr, isShow, col)
+    if nargin < 5, col = []; end
+    injNum = size(tm, 1);
+    n = size(tm, 2);
+    X = nan(injNum,n);
+    Y = nan(injNum,n);
+    AUC = nan(injNum,1);
+    [~,I] = sort(tm,2,'descend');
+    if isShow, figure; end
+    for i=1:injNum
+        r = gmb(i,I(i,:));
+        tc = sum(gmb(i,:));
+        if tc==0
+            disp(['no active ground truth i=' num2str(i)]);
+            continue;
+        end
+        fc = n - tc;
+        x = 0; y = 0;
+        for j=1:n
+            if r(j) > 0
+                y = y + 1/tc;
+            else
+                x = x + 1/fc;
+            end
+            X(i,j) = x;
+            Y(i,j) = y;
+        end
+        AUC(i) = trapz(X(i,:),Y(i,:));
+        if isShow
+            hold on;
+            if isempty(col), plot(X(i,:),Y(i,:));
+            else, plot(X(i,:),Y(i,:),'Color',col); end
+            hold off;
+        end
+    end
+    if isShow
+        xlim([0 1]); ylim([0 1]); daspect([1 1 1]);
+        hold on; plot([0 1], [0 1],':','Color',[0.5 0.5 0.5]); hold off;
+        title(['ROC curve of ' groupStr]);
+    end
+end
