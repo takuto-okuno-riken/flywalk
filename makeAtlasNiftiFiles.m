e229ee03129d40ec3bb5ffcf9338c470cad91b84
takuto-okuno-riken <66657250+takuto-okuno-riken@users.noreply.github.com>
2024-12-01 18:42:14 +0900

    fixed to output ROI atlas with mask.

---
+++ makeAtlasNiftiFiles.m
@@ -0,0 +1,40 @@
+% make Atlas nifti files. This script outputs specific ROI atlas with mask image.
+% need to run makeStructConnectivity.m first.
+
+function makeAtlasNiftiFiles
+    name = 'hemi';
+
+    % read FDA mask
+    minfo = niftiinfo('template/thresholded_FDACal_mask.nii.gz');
+    Vm = niftiread(minfo);
+    Vm(Vm>0) = 1;
+    Vm(Vm<1) = 0;
+
+    % output atlas nifti file
+    fname = 'hemiFlyem52atlasCal';
+    info = niftiinfo(['atlas/' fname '.nii.gz']);
+    V = niftiread(info);
+    idx = find(V>0);
+    aV = Vm;
+    aV(aV>0) = max(V(:)) + 10;
+    aV(idx) = V(idx);
+
+    % output nii file
+    niftiwrite(aV,['atlas/' fname 'wm.nii'],info,'Compressed',true);
+
+    roitypes = {{'hemiBranson7065km',''}, {'hemiCmkm',''}, {'hemiCmkm','r1w1'}, {'hemiDistKm',''}, {'hemiRand',''}};
+    for j=1:length(roitypes)
+        for i=[50]
+            fname = [roitypes{j}{1} num2str(i) roitypes{j}{2} 'atlasCal'];
+            info = niftiinfo(['atlas/' fname '.nii.gz']);
+            V = niftiread(info);
+            idx = find(V>0);
+            aV = Vm;
+            aV(aV>0) = max(V(:)) + 10;
+            aV(idx) = V(idx);
+    
+            % output nii file
+            niftiwrite(aV,['atlas/' fname 'wm.nii'],minfo,'Compressed',true);
+        end
+    end
+end
