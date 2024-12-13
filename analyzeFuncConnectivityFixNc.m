0bcf996e723b90f1cc536194ee6cf40b2ed99952
takuto-okuno-riken <66657250+takuto-okuno-riken@users.noreply.github.com>
2024-12-13 17:10:31 +0900

    fixed ncountMat and sycountMat.

--- analyzeFuncConnectivityFixNc.m
+++ analyzeFuncConnectivityFixNc.m
@@ -1,7 +1,7 @@
 % re-calculation FC-SC of post-synapse data.
 % this script should run after analyzeFuncConnectivity.m.
 
-function analyzeFuncConnectivitySy
+function analyzeFuncConnectivityNc
     %%%%%%%%%%%%%% set parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % pre-process
     preproc = 'ar'; % for move correct, slice time correct
@@ -10,8 +10,8 @@ function analyzeFuncConnectivitySy
     % output time-series (smoothing, highpass filter, nuisance removal)
     hpfTh = [0]; % high-pass filter threshold
 %    hpfTh = [0, 0.1, 0.05, 0.025, 0.02, 0.01, 0.009, 0.008, 0.005, 0.001]; % high-pass filter threshold
-%    smooth = {'', 's10', 's20', 's30', 's40', 's50', 's60', 's70', 's80'};
-    smooth = {''};
+    smooth = {'', 's10', 's20', 's30', 's40', 's50', 's60', 's70', 's80'};
+%    smooth = {''};
     nuisance = {'','gm','gmgs','nui','6hm','6hmgm','6hmgmgs','6hmnui','24hm','24hmgm','24hmgmgs','24hmnui', ... %12
         'acomp','gmacomp','gmgsacomp','tcomp','tacomp', ... %17
         '6hmacomp','6hmgmacomp','6hmgmgsacomp','6hmtcomp','6hmtacomp', ... %22
@@ -48,9 +48,9 @@ function analyzeFuncConnectivitySy
 %    roitypes = {'hemiRoi1','hemiRoi5','hemiRoi7','hemiRoi27','hemiRoi30','hemiRoi32','hemiRoi43','hemiRoi52', ...
 %        'hemiRoi54','hemiRoi57','hemiRoi59','hemiRoi63','hemiRoi65','hemiRoi67','hemiRoi78','hemiRoi82', ...
 %        'hemiRoi89','hemiRoi93','hemiRoi95','hemiRoi100','hemiRoi101','hemiRoi106','hemiRoi113'};
-%    roitypes = {'flyemroi','hemiBranson7065km50','hemiCmkm50','hemiCmkm50r1w1','hemiDistKm50','hemiRand50','hemiVrand50'};
+    roitypes = {'flyemroi','hemiBranson7065km50','hemiCmkm50','hemiCmkm50r1w1','hemiDistKm50','hemiRand50','hemiVrand50'};
 %    roitypes = {'hemiBranson7065km30','hemiCmkm30','hemiCmkm30r1w1','hemiDistKm30','hemiRand30','hemiVrand30'};
-    roitypes = {'hemiCmkm20000','hemiCmkm20000r1w1','hemiDistKm20000','hemiVrand20000'};
+%    roitypes = {'hemiCmkm20000','hemiCmkm20000r1w1','hemiDistKm20000','hemiVrand20000'};
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -63,15 +63,9 @@ function analyzeFcROItype(roitype, preproc, hpfTh, smooth, nuisance, sbjids)
 
     % load structural connectivity matrix (from makeStructConnectivity.m)
     switch(roitype)
-    case 'flyemroi'
-        load('data/neuprint_connectlist.mat');
-        ids = primaryIds;
     case 'flyemroif'
         load('data/neuprint_connectlist.mat');
         ids = 1:roiNum;
-    case 'bransonhemi'
-        load('data/branson_connectlist.mat');
-        ids = primaryIds;
     otherwise
         roitype = lower(roitype);
         load(['data/' roitype '_connectlist.mat']);
@@ -79,10 +73,12 @@ function analyzeFcROItype(roitype, preproc, hpfTh, smooth, nuisance, sbjids)
     end
     if ~exist('ncountMat','var'), disp(['cannot find ncountMat in ' roitype ' ... skip']); return; end
 
-    S = sycountMat(ids,ids,1); 
-    Sb = sycountMat(ids,ids,2);
+    C2 = ncountMat(ids,ids,1); S = sycountMat(ids,ids,1);
+    C2b = ncountMat(ids,ids,2); Sb = sycountMat(ids,ids,2);
 
+    lC2 = log10(C2); lC2(lC2<0) = 0;
     lS = log10(S); lS(lS<0) = 0;
+    lC2b = log10(C2b); lC2b(lC2b<0) = 0;
     lSb = log10(Sb); lSb(lSb<0) = 0;
 
     for h=1:length(hpfTh)
@@ -126,12 +122,20 @@ function analyzeFcROItype(roitype, preproc, hpfTh, smooth, nuisance, sbjids)
                 load(aucmat);
 
                 % full ROIs (vs. mean group data)
+                R(1) = corr(lC2(:),abs(Dmz(:))); % whole vs. m-FCz
+                disp(['prefix=' pftype ' : log10(neurons2) vs. m-FCz = ' num2str(R(1))]);
                 R(3) = corr(lS(:),abs(Dmz(:)));
                 disp(['prefix=' pftype ' : log10(synapses) vs. m-FCz = ' num2str(R(3))]);
+                R(7) = corr(lC2b(:),abs(Dmz(:))); % Traced vs. m-FCz
+                disp(['prefix=' pftype ' : log10(neurons2b) vs. m-FCz = ' num2str(R(7))]);
                 R(9) = corr(lSb(:),abs(Dmz(:)));
                 disp(['prefix=' pftype ' : log10(synapses b) vs. m-FCz = ' num2str(R(9))]);
+                R(13) = corr(lC2(:),abs(T3(:)));
+                disp(['prefix=' pftype ' : log10(neurons2) vs. FC-Tval = ' num2str(R(13))]);
                 R(15) = corr(lS(:),abs(T3(:)));
                 disp(['prefix=' pftype ' : log10(synapses) vs. FC-Tval = ' num2str(R(15))]);
+                R(19) = corr(lC2b(:),abs(T3(:)));
+                disp(['prefix=' pftype ' : log10(neurons2b) vs. FC-Tval = ' num2str(R(19))]);
                 R(21) = corr(lSb(:),abs(T3(:)));
                 disp(['prefix=' pftype ' : log10(synapses b) vs. FC-Tval = ' num2str(R(21))]);
 
@@ -147,19 +151,31 @@ function analyzeFcROItype(roitype, preproc, hpfTh, smooth, nuisance, sbjids)
                 for th = 1:thN
 %                    parfor th = 1:thN
                     % include injection voxel in ground truth
+                    c2th = prctile(C2(C2>0),th-1);
+                    ct2 = C2; ct2(ct2<c2th) = 0; ct2(ct2>0) = 1;
                     sth = prctile(S(S>0),th-1);
                     st = S; st(st<sth) = 0; st(st>0) = 1;
 
+                    c2bth = prctile(C2b(C2b>0),th-1);
+                    ct2b = C2b; ct2b(ct2b<c2bth) = 0; ct2b(ct2b>0) = 1;
                     sbth = prctile(Sb(Sb>0),th-1);
                     stb = Sb; stb(stb<sbth) = 0; stb(stb>0) = 1;
 
                     aucs = aths{th};
+                    [~, ~, auc] = calcShowGroupROCcurve(ct2(:)', abs(Dmz(:)'), ['m-FCz vs. neurons2 th=' num2str(th-1)], false);
+                    aucs{1} = single(auc);
                     [~, ~, auc] = calcShowGroupROCcurve(st(:)', abs(Dmz(:)'), ['m-FCz vs. synapses th=' num2str(th-1)], false);
                     aucs{3} = single(auc);
+                    [X, Y, auc] = calcShowGroupROCcurve(ct2b(:)', abs(Dmz(:)'), ['m-FCz vs. neurons2b th=' num2str(th-1)], false);
+                    aucs{7} = single(auc); % input node is one, so it is not vector.
                     [~, ~, auc] = calcShowGroupROCcurve(stb(:)', abs(Dmz(:)'), ['m-FCz vs. synapses b th=' num2str(th-1)], false);
                     aucs{9} = single(auc);
+                    [~, ~, auc] = calcShowGroupROCcurve(ct2(:)', abs(T3(:)'), ['FC-Tval vs. neurons2 th=' num2str(th-1)], false);
+                    aucs{13} = single(auc);
                     [~, ~, auc] = calcShowGroupROCcurve(st(:)', abs(T3(:)'), ['FC-Tval vs. synapses th=' num2str(th-1)], false);
                     aucs{15} = single(auc);
+                    [~, ~, auc] = calcShowGroupROCcurve(ct2b(:)', abs(T3(:)'), ['FC-Tval vs. neurons2b th=' num2str(th-1)], false);
+                    aucs{19} = single(auc);
                     [~, ~, auc] = calcShowGroupROCcurve(stb(:)', abs(T3(:)'), ['FC-Tval vs. synapses b th=' num2str(th-1)], false);
                     aucs{21} = single(auc);
                     aths{th} = aucs;
