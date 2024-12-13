c13d1b096fbda573d825682972142b09a8233fd6
takuto-okuno-riken <66657250+takuto-okuno-riken@users.noreply.github.com>
2024-12-13 19:19:46 +0900

    separate makeSCcountMatrix functions.

---
+++ makeSCweightMatrixLarge.m
@@ -0,0 +1,30 @@
+% make SC neuron weight matrix for huge matrix by hemibrain FlyEM structure data.
+
+function [weightMat] = makeSCweightMatrixLarge(sycountMat, type)
+    roimax = size(sycountMat,1);
+    Nout = {}; Nin = {};
+    nfile = ['results/cache-' type '_L_Nin_Nout.mat'];
+    load(nfile);
+
+    delete(gcp('nocreate')); % shutdown pools
+    parpool(32);
+
+    % calculate weight matrix (full, neurons, others)
+    weightMat = sparse(roimax,roimax);
+    parfor i=1:roimax
+        if isempty(Nout{i}), continue; end
+        disp(['ROI ' num2str(i) ' / ' num2str(roimax)]);
+        outnids = Nout{i};
+        S = sparse(1,roimax);
+        for j=1:roimax
+            if isempty(Nin{j}), continue; end
+            innids = Nin{j};
+            % find input neuron rate from ROI(i)
+            logi = ismember(innids,outnids);
+            S(j) = double(sum(logi)) / length(innids); % in-weight (from i to j)
+        end
+        weightMat(i,:) = S;
+    end
+    delete(gcp('nocreate')); % shutdown pools
+    weightMat = weightMat .* sycountMat;
+end
