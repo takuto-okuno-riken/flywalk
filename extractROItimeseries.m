ee7f8602bdad3e9199af91a67b65314cd5f0f084
takuto-okuno-riken <66657250+takuto-okuno-riken@users.noreply.github.com>
2024-12-22 21:11:09 +0900

    fixed nuisance, smooth.

--- extractROItimeseries.m
+++ extractROItimeseries.m
@@ -13,7 +13,8 @@ function extractROItimeseries
 %    hpfTh = [0, 0.1, 0.05, 0.025, 0.02, 0.01, 0.009, 0.008, 0.005, 0.001]; % high-pass filter threshold
 %    smooth = {'', 's10', 's20', 's30', 's40', 's50', 's60', 's70', 's80'};
 %    smooth = {'s90', 's100', 's110', 's120', 's130', 's140', 's150', 's160', 's170', 's180', 's190', 's200', 's210', 's220', 's230', 's240', 's250', 's260', 's270', 's280', 's290', 's300'};
-    smooth = {'', 's30', 's80', 's150','s230','s300'};
+%    smooth = {'', 's30', 's80', 's150','s230','s300'};
+    smooth = {'', 's30', 's80', 's150'};
 %    smooth = {''};
     nuisance = {'','gm','gmgs','nui','6hm','6hmgm','6hmgmgs','6hmnui','24hm','24hmgm','24hmgmgs','24hmnui', ... %12
         'acomp','gmacomp','gmgsacomp','tcomp','tacomp', ... %17
@@ -22,7 +23,8 @@ function extractROItimeseries
         'pol','polacomp','poltcomp','poltacomp','polgmtacomp', ...
         '6hmpol','6hmpolacomp','6hmpoltcomp','6hmpoltacomp','6hmpolgmtacomp', };
 %    nuisance = {'6hmtacomp'}; % good for bransonhemi, branson7065km50
-    nuisance = {'','poltcomp'}; % good for DistKm(synapse)
+%    nuisance = {'','poltcomp'}; % good for DistKm(synapse)
+    nuisance = {'','6hm','tcomp','pol','poltcomp'}; % good for hemiRoiXX
 %    nuisance = {''};
 
     % ROI name
