8a6231acdf0c6669e125930a68d6fba9ca7b59e7
takuto-okuno-riken <66657250+takuto-okuno-riken@users.noreply.github.com>
2024-12-23 12:27:42 +0900

    fixed for hemicube4.

--- extractROItimeseries.m
+++ extractROItimeseries.m
@@ -13,8 +13,8 @@ function extractROItimeseries
 %    hpfTh = [0, 0.1, 0.05, 0.025, 0.02, 0.01, 0.009, 0.008, 0.005, 0.001]; % high-pass filter threshold
 %    smooth = {'', 's10', 's20', 's30', 's40', 's50', 's60', 's70', 's80'};
 %    smooth = {'s90', 's100', 's110', 's120', 's130', 's140', 's150', 's160', 's170', 's180', 's190', 's200', 's210', 's220', 's230', 's240', 's250', 's260', 's270', 's280', 's290', 's300'};
-%    smooth = {'', 's30', 's80', 's150','s230','s300'};
-    smooth = {'', 's30', 's80', 's150'};
+    smooth = {'', 's30', 's80', 's150','s230','s300'};
+%    smooth = {'', 's30', 's80', 's150'};
 %    smooth = {''};
     nuisance = {'','gm','gmgs','nui','6hm','6hmgm','6hmgmgs','6hmnui','24hm','24hmgm','24hmgmgs','24hmnui', ... %12
         'acomp','gmacomp','gmgsacomp','tcomp','tacomp', ... %17
@@ -23,8 +23,8 @@ function extractROItimeseries
         'pol','polacomp','poltcomp','poltacomp','polgmtacomp', ...
         '6hmpol','6hmpolacomp','6hmpoltcomp','6hmpoltacomp','6hmpolgmtacomp', };
 %    nuisance = {'6hmtacomp'}; % good for bransonhemi, branson7065km50
-%    nuisance = {'','poltcomp'}; % good for DistKm(synapse)
-    nuisance = {'','6hm','tcomp','pol','poltcomp'}; % good for hemiRoiXX
+    nuisance = {'','poltcomp'}; % good for DistKm(synapse)
+%    nuisance = {'','6hm','tcomp','pol','poltcomp'}; % good for hemiRoiXX
 %    nuisance = {''};
 
     % ROI name
@@ -57,6 +57,7 @@ function extractROItimeseries
 %    roitypes = {'hemiCmkm5000','hemiDistKm5000','hemiCmkm10000','hemiDistKm10000','hemiCmkm20000','hemiDistKm20000'};
 %    roitypes = {'hemiCmkm50','hemiDistKm50','hemiCmkm100','hemiDistKm100','hemiCmkm500','hemiDistKm500'}; % for large smoothing size & no nuisanse, poltcomp
     roitypes = {'hemiRoi68-59-87-106-50-27-54'};
+    roitypes = {'hemiCube4'};
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
